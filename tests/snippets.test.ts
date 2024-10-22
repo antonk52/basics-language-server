6910747a5bbe26ee88027f15d1e8070bd4fa8311
Anton Kastritskiy <antonk52@meta.com>
2024-10-21 17:21:41 -0700

    support multiple match strategies

--- tests/snippets.test.ts
+++ tests/snippets.test.ts
@@ -12,33 +12,84 @@ describe('snippets', () => {
                 body: 'value',
                 description: 'description'
             }
-        ]
+        ],
     ]);
 
-    it('return completion item from snippet name', () => {
-        const expectedCompItems = [
-            {
-                label: 'description',
-                kind: 15,
-                insertText: 'value',
-                insertTextFormat: 2
-            }
-        ];
+    describe('match strategy exact', () => {
+        it('return completion item from snippet name', () => {
+            const expectedCompItems = [
+                {
+                    label: 'description',
+                    kind: 15,
+                    insertText: 'value',
+                    insertTextFormat: 2
+                }
+            ];
 
-        expect(snippetCache.getCompletionItems('javascript', 'pref'))
-            .toEqual(expectedCompItems);
+            expect(snippetCache.getCompletionItems('javascript', 'pref', 'exact'))
+                .toEqual(expectedCompItems);
+        });
+        it('does not return completion item from snippet name with missing characters', () => {
+            expect(snippetCache.getCompletionItems('javascript', 'pex', 'exact'))
+                .toEqual([]);
+        });
+        it('return completion item from snippet prefix', () => {
+            const expectedCompItems = [
+                {
+                    label: 'description',
+                    kind: 15,
+                    insertText: 'value',
+                    insertTextFormat: 2
+                }
+            ];
+
+            expect(snippetCache.getCompletionItems('javascript', 'na', 'exact'))
+                .toEqual(expectedCompItems);
+        });
     });
-    it('return completion item from snippet prefix', () => {
-        const expectedCompItems = [
-            {
-                label: 'description',
-                kind: 15,
-                insertText: 'value',
-                insertTextFormat: 2
-            }
-        ];
+    describe('match strategy fuzzy', () => {
+        it('return completion item from snippet name like exact', () => {
+            const expectedCompItems = [
+                {
+                    label: 'description',
+                    kind: 15,
+                    insertText: 'value',
+                    insertTextFormat: 2
+                }
+            ];
 
-        expect(snippetCache.getCompletionItems('javascript', 'na'))
-            .toEqual(expectedCompItems);
+            expect(snippetCache.getCompletionItems('javascript', 'pref', 'fuzzy'))
+                .toEqual(expectedCompItems);
+        });
+        it('return completion item from snippet name with missing characters', () => {
+            const expectedCompItems = [
+                {
+                    label: 'description',
+                    kind: 15,
+                    insertText: 'value',
+                    insertTextFormat: 2
+                }
+            ];
+
+            expect(snippetCache.getCompletionItems('javascript', 'pex', 'fuzzy'))
+                .toEqual(expectedCompItems);
+        });
+        it('does not return completion item from snippet name with missing FIRST characters', () => {
+            expect(snippetCache.getCompletionItems('javascript', 'refix', 'fuzzy'))
+                .toEqual([]);
+        });
+        it('return completion item from snippet prefix', () => {
+            const expectedCompItems = [
+                {
+                    label: 'description',
+                    kind: 15,
+                    insertText: 'value',
+                    insertTextFormat: 2
+                }
+            ];
+
+            expect(snippetCache.getCompletionItems('javascript', 'na', 'fuzzy'))
+                .toEqual(expectedCompItems);
+        });
     });
 });
